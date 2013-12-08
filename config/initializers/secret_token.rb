133659f3a80265cf4b940a52c6d25a7d5590f1fc
Justin Louie <justin@cmn.com>
2013-12-08 14:46:44 +0800

    initial commit of 30days, here we go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+For30days::Application.config.secret_key_base = '69506a6741d451b2ce767b66c4904e3d181c76bf48cf323139ff0ee5edc1c3e4dd98f5564d7ad1de6b897fff544e207d24d505a17d18c7a368d1d6e4bca6677e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+For30days::Application.config.session_store :cookie_store, key: '_for30days_session'
