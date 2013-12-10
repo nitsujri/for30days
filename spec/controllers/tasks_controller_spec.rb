3b86b75a5effbfd6cd0dc89fb3fda36612a08600
Justin Louie <justin@cmn.com>
2013-12-10 17:28:56 +0800

    task controller testing setup

---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe TasksController do
+
+  describe "Create Task" do
+    it "marks task done for day" do
+
+    end
+
+    it "marked only once per day" do
+
+    end
+
+    it "cannot be marked completed after 30 days" do
+
+    end
+
+    it "cannot be marked while not active" do
+
+    end
+  end
+end
